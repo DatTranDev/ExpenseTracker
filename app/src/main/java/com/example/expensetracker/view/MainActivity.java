2954c9f590ad7b35ba25c5b0038714e8b68e5b4b
quark1412 <hvphuongdts@gmail.com>
2024-04-05 22:08:07 +0700

    feat: create nav bar and fragments

--- app/src/main/java/com/example/expensetracker/view/MainActivity.java
+++ app/src/main/java/com/example/expensetracker/view/MainActivity.java
@@ -1,16 +1,52 @@
 package com.example.expensetracker.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 import com.example.expensetracker.R;
+import com.example.expensetracker.databinding.ActivityMainBinding;
+import com.example.expensetracker.fragment.AccountFragment;
+import com.example.expensetracker.fragment.BudgetFragment;
+import com.example.expensetracker.fragment.FundFragment;
+import com.example.expensetracker.fragment.HomeFragment;
+import com.example.expensetracker.fragment.TransactionFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.navBar.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.home) {
+                replaceFragment(new HomeFragment());
+            } else if (item.getItemId() == R.id.transactionLog) {
+                replaceFragment(new TransactionFragment());
+            } else if (item.getItemId() == R.id.budget) {
+                replaceFragment(new BudgetFragment());
+            } else if (item.getItemId() == R.id.mutualFund) {
+                replaceFragment(new FundFragment());
+            } else if (item.getItemId() == R.id.account) {
+                replaceFragment(new AccountFragment());
+            }
+
+            return true;
+        });
     }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.contentLayout, fragment);
+        fragmentTransaction.commit();
+    }
+
 }
