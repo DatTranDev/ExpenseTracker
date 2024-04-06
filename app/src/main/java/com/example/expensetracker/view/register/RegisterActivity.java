dd9f9d2bc18a660ed7f653f4899588598421d7eb
Tran Thanh Dat <neban0444@gmail.com>
2024-04-07 02:29:25 +0700

    register welcome ui

--- app/src/main/java/com/example/expensetracker/view/register/RegisterActivity.java
+++ app/src/main/java/com/example/expensetracker/view/register/RegisterActivity.java
@@ -2,15 +2,44 @@ package com.example.expensetracker.view.register;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import com.example.expensetracker.R;
+import com.example.expensetracker.view.login.LoginActivity;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    public static Intent newIntent(Context context) {
+        return new Intent(context, RegisterActivity.class);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        EditText editText = findViewById(R.id.editTextTextEmailAddress);
+        editText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    editText.setText("");
+                    editText.setTextColor(getResources().getColor(R.color.black));
+                }
+            }
+        });
+
+        TextView textView = findViewById(R.id.textViewLogin);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
