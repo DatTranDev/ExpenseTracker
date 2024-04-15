ac4cdea3a33179b8b8a1fa29897e79512433cb29
irisus1 <22520210@gm.uit.edu.vn>
2024-04-15 22:21:50 +0700

    account display

--- app/src/main/java/com/example/expensetracker/fragment/AccountFragment.java
+++ app/src/main/java/com/example/expensetracker/fragment/AccountFragment.java
@@ -1,10 +1,14 @@
 package com.example.expensetracker.fragment;
 
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.example.expensetracker.R;
 
@@ -24,7 +28,7 @@ public class AccountFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
+    private LinearLayout walletLayout;
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
@@ -54,12 +58,23 @@ public class AccountFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false);
+        View view = inflater.inflate(R.layout.fragment_account, container, false);
+        walletLayout = view.findViewById(R.id.linearLayout_wallet);
+
+        walletLayout.setOnClickListener(v -> {
+            // event
+        });
+
+        return view;
     }
+
+
+
 }
