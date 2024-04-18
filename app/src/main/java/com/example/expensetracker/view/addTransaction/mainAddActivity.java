569176880c131fbcbbf6bbe3863b3a4354dc221e
duc190604 <haminhduchaminhduc@gmail.com>
2024-04-18 23:26:45 +0700

    update

---
+++ app/src/main/java/com/example/expensetracker/view/addTransaction/mainAddActivity.java
@@ -0,0 +1,13 @@
+package com.example.expensetracker.view.addTransaction;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.expensetracker.R;
+
+public class mainAddActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_transaction);
+    }
+}
