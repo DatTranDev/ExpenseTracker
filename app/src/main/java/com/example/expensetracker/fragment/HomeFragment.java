b2d56926fb6087bb83968b8a0d9aee291bbe4178
irisus1 <22520210@gm.uit.edu.vn>
2024-05-19 23:28:32 +0700

    update logout and account_wallet

--- app/src/main/java/com/example/expensetracker/fragment/HomeFragment.java
+++ app/src/main/java/com/example/expensetracker/fragment/HomeFragment.java
@@ -1,17 +1,16 @@
 package com.example.expensetracker.fragment;
 
 import android.content.Context;
-import android.os.Bundle;
 import android.content.SharedPreferences;
-
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.anychart.AnyChart;
 import com.anychart.AnyChartView;
