6ff6ae3e8b8b1bcf85ee380593b6a1281cda9506
Tran Thanh Dat <neban0444@gmail.com>
2024-05-20 01:56:59 +0700

    load icon, categories

--- app/src/main/java/com/example/expensetracker/view/SplashScreenActivity.java
+++ app/src/main/java/com/example/expensetracker/view/SplashScreenActivity.java
@@ -1,5 +1,6 @@
 package com.example.expensetracker.view;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.widget.Button;
 
@@ -7,7 +8,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.expensetracker.R;
 import com.example.expensetracker.api.ApiCallBack;
+import com.example.expensetracker.model.AppUser;
+import com.example.expensetracker.model.Category;
 import com.example.expensetracker.model.Icon;
+import com.example.expensetracker.repository.AppUserRepository;
 import com.example.expensetracker.repository.IconRepository;
 import com.example.expensetracker.view.register.WelcomeActivity;
 import com.google.gson.Gson;
@@ -20,35 +24,46 @@ public class SplashScreenActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
         Button retry = findViewById(R.id.reloadButton);
+
         retry.setOnClickListener(new android.view.View.OnClickListener() {
             @Override
             public void onClick(android.view.View v) {
-                IconRepository.getInstance().getAllIcons(new ApiCallBack<List<Icon>>() {
-                    @Override
-                    public void onSuccess(List<Icon> response) {
-                        new android.os.Handler().postDelayed(new Runnable() {
-                            public void run() {
-                                android.content.Intent intent = new android.content.Intent(SplashScreenActivity.this, WelcomeActivity.class);
-                                startActivity(intent);
-                                finish();
-                            }
-                        }, 500);
-                    }
-
-                    @Override
-                    public void onError(String message) {
-                        android.widget.Toast.makeText(SplashScreenActivity.this, message, android.widget.Toast.LENGTH_SHORT).show();
-                    }
-                });
+                retry.setVisibility(android.view.View.GONE);
+                Intent intent = getIntent();
+                finish();
+                startActivity(intent);
             }
         });
         IconRepository.getInstance().getAllIcons(new ApiCallBack<List<Icon>>() {
             @Override
             public void onSuccess(List<Icon> response) {
+                //Save icons to shared preferences
                 SharedPreferences sharedPreferences = getSharedPreferences("icons", MODE_PRIVATE);
                 SharedPreferences.Editor editor = sharedPreferences.edit();
                 editor.putString("icons", new Gson().toJson(response));
                 editor.apply();
+
+                //Get categories if user is logged in
+                SharedPreferences sharedPreferencesUs = getSharedPreferences("user", MODE_PRIVATE);
+                String userJson = sharedPreferencesUs.getString("user", null);
+                AppUser appUser = new AppUser();
+                if (userJson != null) {
+                    appUser = new Gson().fromJson(userJson, AppUser.class);
+                    AppUserRepository.getInstance().getCategory(appUser.getId(), new ApiCallBack<List<Category>>() {
+                        @Override
+                        public void onSuccess(List<Category> response) {
+                            SharedPreferences sharedPreferences = getSharedPreferences("categories", MODE_PRIVATE);
+                            SharedPreferences.Editor editor = sharedPreferences.edit();
+                            editor.putString("categories", new Gson().toJson(response));
+                            editor.apply();
+                        }
+                        @Override
+                        public void onError(String message) {
+                            android.widget.Toast.makeText(SplashScreenActivity.this, message, android.widget.Toast.LENGTH_SHORT).show();
+                            //retry.setVisibility(android.view.View.VISIBLE);
+                        }
+                    });
+                }
                 new android.os.Handler().postDelayed(new Runnable() {
                     public void run() {
                         android.content.Intent intent = new android.content.Intent(SplashScreenActivity.this, WelcomeActivity.class);
