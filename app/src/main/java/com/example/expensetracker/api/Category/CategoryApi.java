b1bb47b4a3c14787c42c6d31d874847ada7984c1
Tran Thanh Dat <neban0444@gmail.com>
2024-05-30 23:34:33 +0700

    update category

--- app/src/main/java/com/example/expensetracker/api/Category/CategoryApi.java
+++ app/src/main/java/com/example/expensetracker/api/Category/CategoryApi.java
@@ -8,11 +8,15 @@ import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.DELETE;
 import retrofit2.http.HTTP;
+import retrofit2.http.PATCH;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface CategoryApi {
     @POST("category/add")
     Call<DataResponse<Category>> addCategory(@Body CategoryReq categoryReq);
     @HTTP(method = "DELETE", path = "category/delete", hasBody = true)
     Call<DataResponse<Category>> deleteCategory(@Body UserCategory category);
+    @PATCH("category/update/{id}")
+    Call<DataResponse<Category>> updateCategory(@Path("id") String id, @Body CategoryReq categoryReq);
 }
